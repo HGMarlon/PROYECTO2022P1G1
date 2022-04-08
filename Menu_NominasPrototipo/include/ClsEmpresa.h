2b4ad078da208db475811b19420467cab6b899fd
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-04-08 17:23:56 -0600

    definicion atributos de clase empresa

--- Menu_NominasPrototipo/include/ClsEmpresa.h
+++ Menu_NominasPrototipo/include/ClsEmpresa.h
@@ -1,16 +1,34 @@
+//librerias
+#include<string>
+
+//cpp
 #ifndef CLSEMPRESA_H
 #define CLSEMPRESA_H
 
+using namespace std;
 
 class ClsEmpresa
 {
     public:
         ClsEmpresa();
+
+
+
         virtual ~ClsEmpresa();
 
     protected:
 
     private:
+        char m_sNombreEmpresa[20];
+        char m_sCorreoEmpresa[20];
+        char m_sNombreRepresentante[20];
+        char m_sActividadEconomica[20];
+        int m_iNitEmpresa[20];
+        int m_iDireccionEmpresa[20];
+        int m_iTelefonoEmpresa[20];
+        int m_iNumeroEmpleados[20];
+        int m_iNumeroDeEmpresa[20];
+
 };
 
 #endif // CLSEMPRESA_H
