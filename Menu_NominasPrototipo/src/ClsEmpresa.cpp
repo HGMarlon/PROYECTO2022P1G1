89563fb7c58e44f033c089fec28db134c42011cf
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-04-08 22:32:40 -0600

    creacion menu clase empresa

--- Menu_NominasPrototipo/src/ClsEmpresa.cpp
+++ Menu_NominasPrototipo/src/ClsEmpresa.cpp
@@ -27,14 +27,57 @@ ClsEmpresa::ClsEmpresa(char p_sNombreEmpresa,char p_sCorreoEmpresa,char p_sNombr
 }
 
 //metodo obtiene nombre de la empresa
+void ClsEmpresa::mMenuEmpresa(){
+    int iSeleccionMenuEmpresa;
+    do
+    {
+	system("cls");
 
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t |   SISTEMA GESTION EMPRESA |"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t 1. Ingreso Empresa"<<endl;
+	cout<<"\t\t\t 2. Despliegue Empresa"<<endl;
+	cout<<"\t\t\t 3. Modifica Empresa"<<endl;
+	cout<<"\t\t\t 4. Busca Empresa"<<endl;
+	cout<<"\t\t\t 5. Borra Empresa"<<endl;
+	cout<<"\t\t\t 0. Volver al menu superior"<<endl;
 
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/0]"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>iSeleccionMenuEmpresa;
 
-
-
-
-
-
+    switch(iSeleccionMenuEmpresa)
+    {
+	case 1:
+		mostrarE.mIngresarEmpresa();
+		getch();
+		break;
+	case 2:
+		desplegarE.mDesplegarEmpresa();
+		getch();
+		break;
+	case 3:
+		modificarE.mModificarEmpresa();
+		getch();
+		break;
+	case 4:
+		buscarE.mBuscarEmpresa();
+		break;
+	case 5:
+	    borrarE.mEliminarEmpresa();
+		break;
+    case 0:
+		break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+		getch();
+	}
+	//getch();
+    }while(iSeleccionMenuEmpresa!=0);
+}
 
 
 ClsEmpresa::~ClsEmpresa()
